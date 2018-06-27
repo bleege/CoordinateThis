ea12d14bc3d506b470e2e1f8a7f6a2443d2fbdb0
Brad Leege <bleege@gmail.com>
2018-06-27 18:05:10 -0500

    Initial Commit

---
+++ CoordinateThis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinateThis
+//
+//  Created by Brad Leege on 6/27/18.
+//  Copyright © 2018 Brad Leege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
